ea935fc2befd516e3dd3293c08d5558781827393
Szymon Kolasa <szkolasa@us.edu.pl>
2017-01-12 18:16:51 +0100

    Adding empty solution

---
+++ NJPO.UnitTest/NJPO.UnitTest.QuadraticEquation/QuadraticEquation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NJPO.UnitTest.QuadraticEquation
+{
+    public struct QuadraticEquation
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public QuadraticEquation(double a = 0, double b = 0, double c = 0)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public double Delta()
+        {
+            return Math.Pow(B, 2) - (4 * A * C);
+        }
+
+        public void ZerosOfFunction(out double? x1, out double? x2)
+        {
+            x1 = x2 = null;
+
+            var delta = Delta();
+
+            if (delta < 0)
+            {
+                return;
+            }
+            else if (delta == 0)
+            {
+                x1 = -B / (2 * A);
+                return;
+            }
+            else
+            {
+                x1 = (-B + Math.Sqrt(delta)) / (2 * A);
+                x2 = (-B - Math.Sqrt(delta)) / (2 * A);
+                return;
+            }
+        }
+    }
+}
---
+++ NJPO.UnitTest/NJPO.UnitTest.UnitTests/QuadraticEquationTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NJPO.UnitTest;
+
+namespace NJPO.UnitTest.UnitTests
+{
+    [TestClass]
+    public class QuadraticEquationTests
+    {
+        private QuadraticEquation.QuadraticEquation _quadraticEquation;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _quadraticEquation = new QuadraticEquation.QuadraticEquation();
+        }
+
+        [TestMethod]
+        public void ShouldReturnOneZero()
+        {
+            // Arrange
+            _quadraticEquation.A = 4;
+            _quadraticEquation.B = 4;
+            _quadraticEquation.C = 1;
+
+            double? x1, x2;
+
+            // Act
+            _quadraticEquation.ZerosOfFunction(out x1, out x2);
+
+            // Assert
+            Assert.IsNotNull(x1);
+            Assert.IsNull(x2);
+        }
+    }
+}
